077adac6c5352458ad76aeb8aafa3b360f556afa
Ranveer240 <87566637+Ranveer240@users.noreply.github.com>
2021-10-19 23:12:51 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,6 +1,6 @@
 function login(){
-    player1_name=document.getElementById("player1_name").value;
-    player2_name=document.getElementById("player2_name").value;
+    player1_name=document.getElementById("player1-name").value;
+    player2_name=document.getElementById("player2-name").value;
     localStorage.setItem("player1",player1_name);
     localStorage.setItem("player2",player2_name);
     window.location="game_page.html";
